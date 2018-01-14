4df5faa9675adfdd734fed505aea1ad166fa7ce4
Leonid Petrov <leonid_petrov@imac.local>
2018-01-14 18:23:48 +0300

    Initial Commit

---
+++ Discogs Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Discogs Client
+//
+//  Created by Leonid Petrov on 14/01/2018.
+//  Copyright © 2018 Leonid Petrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
