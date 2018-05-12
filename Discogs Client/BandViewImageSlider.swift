34b6992633b452fcb6ea5ea4bfa39c47b5c6ace6
twiztedklaun <twiztedklaun@users.noreply.github.com>
2018-05-12 16:28:42 +0300

    New commit

---
+++ Discogs Client/BandViewImageSlider.swift
@@ -0,0 +1,14 @@
+//
+//  BandViewImageSlider.swift
+//  Discogs Client
+//
+//  Created by Leonid Petrov on 07/02/2018.
+//  Copyright © 2018 Leonid Petrov. All rights reserved.
+//
+
+import UIKit
+
+class BandViewImageSlider: UIViewController {
+
+
+}
